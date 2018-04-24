3d19e4297156caf98defac881f4f13dec869028b
deepak <er.deepakverma4u@gmail.com>
2018-04-24 18:55:58 +0530

    added stateful widget with one button

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,55 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "My App",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Home Page")
+      home: new HomePage()
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+String myText = "Hello World!";
+
+ void _onButtonPress(){ 
+  setState(() {
+    if(myText.startsWith("H")){
+      myText = "Welcome to my App";
+    }else{
+      myText = "Hello World!";
+    }
+      
+    });
+} 
+
+  Widget _bodyWidget(){
+    return new Container(
+      padding: const EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Column(
+          children: <Widget>[
+            new Text(myText),
+            new RaisedButton(
+              child: new Text("Click"),
+              onPressed: _onButtonPress,
+            )
+          ],
         ),
-        body: new Center(child: new Text("Hello World!"))
       )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("StateFul Demo")
+      ),
+      body: _bodyWidget()
     );
   }
 }
